4a6096424aae0a981fb3444e3e4a2ac3bde8f72b
Dibya Ranjan Samal <dibyaranjan0217@gmai.com>
2026-07-25 18:40:21 +0530

    feat: initialize Spring Boot project

---
+++ src/main/java/com/dibya/knowledgehub/KnowledgeHubApplication.java
@@ -0,0 +1,13 @@
+package com.dibya.knowledgehub.knowledgehub_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgehubAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgehubAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dibya/knowledgehub/KnowledgeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibya.knowledgehub.knowledgehub_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgehubAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
